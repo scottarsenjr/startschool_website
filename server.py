e7d8e9abf9da72b03a6bcc25f9257e50a3f31af3
scxrchy <71168355+scottarsenjr@users.noreply.github.com>
2023-03-25 02:22:53 +0300

    Commit

--- server.py
+++ server.py
@@ -15,15 +15,30 @@ def videos():
     return render_template("videos.html")
 
 
+@app.route('/videos/eng')
+def videos_eng():
+    return render_template("videos-english.html")
+
+
+@app.route('/videos/rus')
+def videos_rus():
+    return render_template("videos-russian.html")
+
+
+@app.route('/videos/math')
+def videos_math():
+    return render_template("videos-math.html")
+
+
 @app.route('/game')
 def game():
     return render_template("game.html")
 
 
-@app.route('/home')
+@app.route('/literature')
 def literature():
     return render_template("literature.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+     app.run(debug=True ,port=8080,use_reloader=False)
